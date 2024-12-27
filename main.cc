d89771d57ab9c3bf49183e39fef193babee9ca89
Magpiny <magpinyb@gmail.com>
2024-12-27 14:18:47 +0300

    FIXING: Runtime build ADDED: Looking for ./config.son file instead of ../config.json

--- main.cc
+++ main.cc
@@ -9,7 +9,7 @@ int main()
     // Set HTTP listener address and port
     drogon::app().addListener("0.0.0.0", PORT);
     // Load config file
-    drogon::app().loadConfigFile("../config.json");
+    drogon::app().loadConfigFile("./config.json");
     // drogon::app().loadConfigFile("../config.yaml");
     // Run HTTP framework,the method will block in the internal event loop
 
